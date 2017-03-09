7256d5cee05e711114c4b77b6b62e58f14558466
Rick Peyton <peytorb@gmail.com>
2017-03-09 16:28:35 -0600

    adding to the test wuite

--- src/parser.py
+++ src/parser.py
@@ -3,11 +3,74 @@ from __future__ import print_function
 import requests
 from bs4 import BeautifulSoup
 
+fake = {
+    "title": "5 Stones Church  - Weekend Messages",
+    "url": "http://www.5stoneschurch.com",
+    "email": "media@5stoneschurch.com",
+    "description": "A life-giving church in Franklin, TN. Follow Jesus - Experience Freedom - Discover Purpose - Make a Difference. 10 AM - Freedom Intermediate School",
+    "messages": [{
+        "order": 1,
+        "title": "Doors - Part 1",
+        "published_date": "Sun, 05 Mar 2017 20:15:44 +0000",
+        "date": "MAR 05, 2017",
+        "file": "http://traffic.libsyn.com/5stoneschurch/20170305_-_Doors_Part_1.mp3",
+        "length": "28:10"
+    }, {
+        "order": 2,
+        "title": "Love Strong - Part 4",
+        "published_date": "Sun, 05 Mar 2017 20:14:29 +0000",
+        "date": "FEB 26, 2017",
+        "file": "http://traffic.libsyn.com/5stoneschurch/20170226_-_Love_Strong_Part_4.mp3",
+        "length": "32:48"
+    }]
+}
+
+
 def lambda_handler(event, context):
     response = send_request()
     parsed_xml = BeautifulSoup(response.text, 'lxml-xml')
+    meta = Meta(parsed_xml)
+    messages = parse_messages(parsed_xml)
 
-    return parsed_xml
+    return fake
+
+
+def parse_messages(response):
+    messages = []
+    messages.append()
+
+    return [message]
+
+
+class Meta(object):
+
+    def __init__(self, response):
+        self.title = "5 Stones Church  - Weekend Messages"
+        self.url = "http://www.5stoneschurch.com"
+        self.email = "media@5stoneschurch.com"
+        self.description = "A life-giving church in Franklin, TN. Follow Jesus - Experience Freedom - Discover Purpose - Make a Difference. 10 AM - Freedom Intermediate School"
+
+
+class Message(object):
+
+    def __init__(self, response):
+        self.order = 1
+        self.title = "Doors - Part 1"
+        self.published_date = "Sun, 05 Mar 2017 20:15:44 +0000"
+        self.date = "MAR 05, 2017"
+        self.file = "http://traffic.libsyn.com/5stoneschurch/20170305_-_Doors_Part_1.mp3"
+        self.length = "28:10"
+
+    def as_dict(self):
+        return {
+            "order": self.order,
+            "title": self.title,
+            "published_date": self.published_date, 05 Mar 2017 20:15:44 +0000",
+            "date": self.date, 2017",
+            "file": self.file,
+            "length": self.length
+        }
+
 
 def send_request():
 
--- tests/test_parser.py
+++ tests/test_parser.py
@@ -12,10 +12,40 @@ class LambdaHanderTest(unittest.TestCase):
         del self.fixture
 
     def test_response_type(self):
-        self.assertIsInstance(self.fixture, str)
+        self.assertIsInstance(self.fixture, dict)
 
-    def test_response_value(self):
-        self.assertEqual(self.fixture, "Got Here")
+    def test_response_title_type(self):
+        self.assertIsInstance(self.fixture["title"], str)
+
+    def test_response_url_type(self):
+        self.assertIsInstance(self.fixture["url"], str)
+
+    def test_response_email_type(self):
+        self.assertIsInstance(self.fixture["email"], str)
+
+    def test_response_description_type(self):
+        self.assertIsInstance(self.fixture["description"], str)
+
+    def test_response_messages_type(self):
+        self.assertIsInstance(self.fixture["messages"], list)
+
+    def test_response_message_order_type(self):
+        self.assertIsInstance(self.fixture["messages"][0]["order"], int)
+
+    def test_response_message_title_type(self):
+        self.assertIsInstance(self.fixture["messages"][0]["title"], str)
+
+    def test_response_message_published_date_type(self):
+        self.assertIsInstance(self.fixture["messages"][0]["published_date"], str)
+
+    def test_response_message_date_type(self):
+        self.assertIsInstance(self.fixture["messages"][0]["date"], str)
+
+    def test_response_message_file_type(self):
+        self.assertIsInstance(self.fixture["messages"][0]["file"], str)
+
+    def test_response_message_length_type(self):
+        self.assertIsInstance(self.fixture["messages"][0]["length"], str)
 
 if __name__ == '__main__':
     unittest.main()
